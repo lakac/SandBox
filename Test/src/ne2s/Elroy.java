d60ecc4d916312842d5b42dc06c5fa1011d0c9e7
nessimlegendre <nessimlegendre@dhcp-104-192.invite.emn.fr>
2016-04-20 14:54:21 +0200

    aaa

--- Test/src/ne2s/Elroy.java
+++ Test/src/ne2s/Elroy.java
@@ -5,6 +5,7 @@ public class Elroy {
 	public static void main(String[] args) {
 	  System.out.println("cartel");
 	  System.out.println("ok");
+	  System.out.println("tranquillement quoi");
 	}
 
 }
